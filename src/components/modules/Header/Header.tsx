7d54009bf3dc8dec6a9784a08f0bf1be575263c0
Ing. Benito Enríquez Mora <enriquez.ico@gmail.com>
2022-08-09 19:15:43 -0500

    feat(add-header): config bootstrap v5.2 and create module header.

--- src/components/modules/Header/Header.tsx
+++ src/components/modules/Header/Header.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import './styles.css';
+
 import Navigation from '../Navigation/Navigation';
 
 const Header = () => {
 	return (
-		<div>
-			Header area
+		<div className='p-4 p-md-5 mb-4 text-white bg-dark-aqua'>
+			<img
+				src='rickandmorty-logo.png'
+				className='rounded mx-auto d-block'
+				alt='Rick and Morty'
+			></img>
 			<Navigation />
 		</div>
 	);
